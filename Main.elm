e9b6bf705f1ab2c778f478bc164c1bd93bb91997
Fred Yankowski <fcy@imsa.edu>
2016-11-11 14:07:54 -0600

    build summary structure

--- Main.elm
+++ Main.elm
@@ -18,14 +18,14 @@ type alias Data =
     List (Maybe Datum)
 
 
-type alias CleanedObj =
+type alias Cleaned =
     Dict Int (Dict String (List String))
 
 
 type alias Model =
     { error : Maybe Http.Error
     , response : Maybe Data
-    , cleaned : CleanedObj
+    , cleaned : Cleaned
     }
 
 
@@ -86,16 +86,40 @@ update msg model =
         Response dataResult ->
             case dataResult of
                 Ok data ->
-                    { model | response = Just data } ! []
+                    { model | response = Just data, cleaned = clean data } ! []
 
                 Err error ->
                     { model | error = Just error } ! []
 
 
+clean : Data -> Cleaned
+clean data =
+    let
+        accum : Maybe Datum -> Cleaned -> Cleaned
+        accum datumMaybe dict =
+            case datumMaybe of
+                Just datum ->
+                    Dict.update datum.categoryId (insertDatum datum) dict
+
+                Nothing ->
+                    dict
+
+        insertDatum datum dict2Maybe =
+            Just <| Dict.update datum.group (insert2 datum) (Maybe.withDefault Dict.empty dict2Maybe)
+
+        insert2 datum listMaybe =
+            Just <| datum.item :: Maybe.withDefault [] listMaybe
+    in
+        List.foldl accum Dict.empty data
+
+
 view : Model -> Html Msg
 view model =
     Html.div []
-        [ detailView model, rawView model ]
+        [ -- detailView model
+          cleanedView model
+          --, rawView model
+        ]
 
 
 detailView model =
@@ -105,6 +129,23 @@ detailView model =
             Html.li [] [ Html.text (toString datum) ]
     in
         Html.div [] (List.map datumView (Maybe.withDefault [] model.response))
+
+
+cleanedView model =
+    let
+        viewItem ( k, v ) =
+            Html.div []
+                [ Html.h3 [] [ Html.text (toString k) ]
+                , Html.div [] (List.map viewItem2 (Dict.toList v))
+                ]
+
+        viewItem2 ( k, v ) =
+            Html.div []
+                [ Html.h4 [] [ Html.text k ]
+                , Html.text (toString v)
+                ]
+    in
+        Html.div [] (List.map viewItem (Dict.toList model.cleaned))
 
 
 rawView model =
